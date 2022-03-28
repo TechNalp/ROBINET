a0cfc573e12b55b308171ebb0f90ea1325f0ccb5
Mathis Planchet <pro.planchet@gmail.com>
2022-03-29 01:14:22 +0200

    Exercice 1 par Yoann
    Publié par moi car impossible par Yoann

--- Jaffre_Julien__De_Kergariou_Yoann__Planchet_Mathis__Louis_Glenn/Robi/src/exercice1/Exercice1_0.java
+++ Jaffre_Julien__De_Kergariou_Yoann__Planchet_Mathis__Louis_Glenn/Robi/src/exercice1/Exercice1_0.java
@@ -1,6 +1,9 @@
 package exercice1;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Point;
+
 import graphicLayer.GRect;
 import graphicLayer.GSpace;
 
@@ -11,6 +14,39 @@ public class Exercice1_0 {
 	public Exercice1_0() {
 		space.addElement(robi);
 		space.open();
+		
+		
+		//D�placement de robi jusqu�au bord droit
+		while(robi.getX()<space.getBounds().getWidth()-robi.getWidth()-1) {
+			robi.translate(new Point(1,0));			
+			try {
+				Thread.sleep(50);
+			}catch(InterruptedException e) {}
+		}
+		//D�placement jusqu�au bord bas
+		while(robi.getY()<space.getBounds().getHeight()-robi.getHeight()-1) {
+			robi.translate(new Point(0,1));			
+			try {
+				Thread.sleep(50);
+			}catch(InterruptedException e) {}
+		}
+		//D�placement jusqu�au bord gauche
+		while(robi.getX()>0) {
+			robi.translate(new Point(-1,0));			
+			try {
+				Thread.sleep(50);
+			}catch(InterruptedException e) {}
+		}
+		//D�placement jusqu�au bord haut
+		
+		while(robi.getY()>0) {
+			robi.translate(new Point(0,-1));			
+			try {
+				Thread.sleep(50);
+			}catch(InterruptedException e) {}
+		}
+		robi.setColor(new Color((int) (Math.random() * 0x1000000)));
+		
 	}
 
 	public static void main(String[] args) {
