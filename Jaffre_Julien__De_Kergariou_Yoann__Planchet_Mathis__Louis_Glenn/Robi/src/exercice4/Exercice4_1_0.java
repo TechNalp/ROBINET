33f5953e0528bafced4d5ea608cda4164b1e304a
Mathis Planchet <pro.planchet@gmail.com>
2022-03-29 16:11:58 +0200

    Exercice 4.1 par Mathis

--- Jaffre_Julien__De_Kergariou_Yoann__Planchet_Mathis__Louis_Glenn/Robi/src/exercice4/Exercice4_1_0.java
+++ Jaffre_Julien__De_Kergariou_Yoann__Planchet_Mathis__Louis_Glenn/Robi/src/exercice4/Exercice4_1_0.java
@@ -1,5 +1,7 @@
 package exercice4;
 
+import java.awt.Color;
+
 // 
 //	(space setColor black)  
 //	(robi setColor yellow) 
@@ -18,9 +20,12 @@ package exercice4;
 //
 
 import java.awt.Dimension;
+import java.awt.Point;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import graphicLayer.GRect;
 import graphicLayer.GSpace;
@@ -43,11 +48,16 @@ public class Exercice4_1_0 {
 		Reference spaceRef = new Reference(space);
 		Reference robiRef = new Reference(robi);
 
+		
 		// Initialisation des references : on leur ajoute les primitives qu'elles
 		// comprenent
-		//
-		// <A VOUS DE CODER>
-		//
+		spaceRef.addCommand("setColor", new GSpaceSetColor());
+		spaceRef.addCommand("sleep", new GSpaceSleep());
+		
+		robiRef.addCommand("setColor", new GRectSetColor());
+		robiRef.addCommand("translate", new GRectTranslate());
+		
+	
 
 		// Enrigestrement des references dans l'environement par leur nom
 		environment.addReference("space", spaceRef);
@@ -92,5 +102,109 @@ public class Exercice4_1_0 {
 	public static void main(String[] args) {
 		new Exercice4_1_0();
 	}
+	
+	public interface Command {
+		abstract public Reference run(Reference receiver, SNode method);
+	}
+	
+	public class Reference{
+		Object receiver;
+		Map<String, Command> primitives;
+		
+		public Reference(Object receiver) {
+			this.receiver = receiver;
+			primitives = new HashMap<String,Command>();
+		}
+		
+		public Command getCommandByName(String selector) {
+			return this.primitives.get(selector);
+		}
+		
+		public void addCommand(String selector,Command primitive) {
+			if(!this.primitives.containsKey(selector)) {
+				this.primitives.put(selector, primitive);
+			}
+		}
+		
+		public Reference run(SNode method) {
+			return this.getCommandByName(method.get(1).contents()).run(this, method);
+			
+		}
+		
+	}
+	
+	public class Environment{
+		HashMap<String,Reference> variables;
+		
+		public Environment(){
+			this.variables = new HashMap<String,Reference>();
+		}
+		
+		public void addReference(String name, Reference ref) {
+			if(!this.variables.containsKey(name)) {
+				this.variables.put(name, ref);
+			}
+		}
+		
+		public Reference getReferenceByName(String name) {
+			return this.variables.get(name);
+		}
+		
+	}
 
+
+	public class GSpaceSetColor implements Command{
+		@Override
+		public Reference run(Reference receiver, SNode method) {
+			try {
+				((GSpace)(receiver.receiver)).setColor((Color)(Color.class.getDeclaredField(method.get(2).contents()).get(null)));
+			} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+
+				e.printStackTrace();
+			}
+			return receiver;
+		}
+		
+	}
+	
+	public class GRectSetColor implements Command{
+		
+		@Override
+		public Reference run(Reference receiver, SNode method) {
+			try {
+				((GRect)(receiver.receiver)).setColor((Color)(Color.class.getDeclaredField(method.get(2).contents()).get(null)));
+			} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+				e.printStackTrace();
+			}
+			return receiver;
+		}
+	}
+	
+	
+	public class GSpaceSleep implements Command{
+		
+		@Override
+		public Reference run(Reference receiver, SNode method) {
+				try {
+					Thread.sleep(Integer.valueOf(method.get(2).contents()));
+				} catch (NumberFormatException | InterruptedException e) {
+					e.printStackTrace();
+				}
+			return receiver;
+		}
+	}
+	
+	public class GRectTranslate implements Command{
+		@Override
+		public Reference run(Reference receiver, SNode method) {
+			int dx = Integer.valueOf(method.get(2).contents());
+			int dy = Integer.valueOf(method.get(3).contents());
+			((GRect)receiver.receiver).translate(new Point(dx,dy));
+			return receiver;
+		}
+	}
+	
+	
 }
+
+
