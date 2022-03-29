3149d4a5f5a7ddfb0c7173a649a29c546eedacae
Julien <julienjaffre1@gmail.com>
2022-03-29 10:12:42 +0200

    Exo 1
    Version de Julien de l'exo 1

--- Jaffre_Julien__De_Kergariou_Yoann__Planchet_Mathis__Louis_Glenn/Robi/src/exercice1/Exercice1_0.java
+++ Jaffre_Julien__De_Kergariou_Yoann__Planchet_Mathis__Louis_Glenn/Robi/src/exercice1/Exercice1_0.java
@@ -1,6 +1,9 @@
 package exercice1;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Point;
+
 import graphicLayer.GRect;
 import graphicLayer.GSpace;
 
@@ -11,6 +14,56 @@ public class Exercice1_0 {
 	public Exercice1_0() {
 		space.addElement(robi);
 		space.open();
+		Dimension dim = space.getSize();
+		robi.setColor(new Color((int) (Math.random() * 0x1000000)));
+		
+		//Déplacement vers la droite
+		Point gap = new Point(1, 0);
+		for(int i = 0; i < dim.width; i++) {
+			robi.translate(gap);
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		//Déplacement vers le bas
+		gap = new Point(0, 1);
+		for(int i = 0; i < dim.height; i++) {
+			robi.translate(gap);
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		//Déplacement vers la gauche
+		gap = new Point(-1, 0);
+		for(int i = 0; i < dim.width; i++) {
+			robi.translate(gap);
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		//Déplacement vers le baut
+		gap = new Point(0, -1);
+		for(int i = 0; i < dim.height; i++) {
+			robi.translate(gap);
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public static void main(String[] args) {
